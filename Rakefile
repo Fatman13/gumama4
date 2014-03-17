60446f9b9a8a43a1a528a2606bdb62a8a6c787da
fatman13 <tctctcly@gmail.com>
2014-03-17 18:07:07 +0800

    1st commit

---
+++ Rakefile
@@ -0,0 +1,20 @@
+# Add your own tasks in files placed in lib/tasks ending in .rake,
+# for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
+
+require File.expand_path('../config/application', __FILE__)
+
+Gumama4::Application.load_tasks
+
+task non_digested: :environment do
+  assets = Dir.glob(File.join(Rails.root, 'public/assets/**/*'))
+  regex = /(-{1}[a-z0-9]{32}*\.{1}){1}/
+  assets.each do |file|
+    next if File.directory?(file) || file !~ regex
+
+    source = file.split('/')
+    source.push(source.pop.gsub(regex, '.'))
+
+    non_digested = File.join(source)
+    FileUtils.cp(file, non_digested)
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gumama4::Application.config.secret_key_base = '4846d6b893b486e40b650ffb9eefb511ad2d607928ac99914925432fbdbcd2d8ab781c001bae9235ef5423088a40f5cbcd253e748fff9afb86d18933a8c3eb63'
